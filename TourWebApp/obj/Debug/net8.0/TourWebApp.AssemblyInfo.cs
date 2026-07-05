2795c3872ce5039b0f103faacb52309210a4453e
Mai-Duy-04 <maiduyy09@gmail.com>
2026-07-06 00:15:15 +0700

    Update project code

--- TourWebApp/obj/Debug/net8.0/TourWebApp.AssemblyInfo.cs
+++ TourWebApp/obj/Debug/net8.0/TourWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TourWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+982229243be913a5e9574e92d5867cb9bdd2911a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62e56b5b4c2bcbf3df75b6c4c112078af398924d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TourWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TourWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
